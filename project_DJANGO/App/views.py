fcbf2a95fd3c89ad8554129e3e6e750bbd07fb30
TomiAltero <tomasaltero@gmail.com>
2023-12-02 20:30:36 -0300

    50% index dinamico

--- project_DJANGO/App/views.py
+++ project_DJANGO/App/views.py
@@ -5,7 +5,8 @@ from .models import *
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        guitars = Guitarra.objects.filter(color = 'Silver')[0:3]
-        print(guitars)
+        silver_collection = Guitarra.objects.filter(color='Silver')[:3]
+        strat_american_special = Guitarra.objects.filter(marca__nombre='Fender', sub_modelo__nombre='American Special')[0]
+        fender_guitars = Guitarra.objects.filter(marca__nombre='Fender').exclude(sub_modelo__nombre='American Special').exclude(color='Silver')[:4]
 
-        return render(request, "index.html", {'guitars': guitars})
+        return render(request, "index.html", {'silver_collection': silver_collection, 'fender_guitars': fender_guitars, 'strat_american_special': strat_american_special})
