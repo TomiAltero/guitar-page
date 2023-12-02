c96af3fcd4b6e431b9ac979785eefbe7e99c52a3
TomiAltero <tomasaltero@gmail.com>
2023-12-01 21:49:57 -0300

    cambios

--- project_DJANGO/App/models.py
+++ project_DJANGO/App/models.py
@@ -43,6 +43,7 @@ class Usuario(models.Model):
 class Marca(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=200)
+    pais_origen = models.ForeignKey(Pais, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.nombre
