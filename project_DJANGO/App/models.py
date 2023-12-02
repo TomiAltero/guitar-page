4013f55cc57d751a4cd9025d1ede2ba8bbf876db
TomiAltero <tomasaltero@gmail.com>
2023-12-02 01:24:30 -0300

    configuracion mysql

--- project_DJANGO/App/models.py
+++ project_DJANGO/App/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Pais(models.Model):
     nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200, default='Descripción del país')
     
     def __str__(self):
         return self.nombre
