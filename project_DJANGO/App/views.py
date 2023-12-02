f268d3de2dd7e1dd59f07ace3365883cd47002d0
TomiAltero <tomasaltero@gmail.com>
2023-12-02 20:33:44 -0300

    50% index dinamico, correccion silver collection

--- project_DJANGO/App/views.py
+++ project_DJANGO/App/views.py
@@ -9,4 +9,7 @@ class IndexView(View):
         strat_american_special = Guitarra.objects.filter(marca__nombre='Fender', sub_modelo__nombre='American Special')[0]
         fender_guitars = Guitarra.objects.filter(marca__nombre='Fender').exclude(sub_modelo__nombre='American Special').exclude(color='Silver')[:4]
 
+        print(silver_collection)
+
+
         return render(request, "index.html", {'silver_collection': silver_collection, 'fender_guitars': fender_guitars, 'strat_american_special': strat_american_special})
