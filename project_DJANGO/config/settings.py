9e88739a42233a57bc3ec768c01d0c4b1c461abb
TomiAltero <tomasaltero@gmail.com>
2023-12-01 23:55:49 -0300

    cambio en la configuracion de la BD

--- project_DJANGO/config/settings.py
+++ project_DJANGO/config/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guitar_page',
+        'USER': 'root',
+        'PASSWORD': 't29092005a',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
